e8187286922ef07bcc3d0623c910b28130586c66
Yong Du <yong.du@nordnet.se>
2022-04-09 17:57:23 +0200

    Update cloudruns.tf

--- cloudruns.tf
+++ cloudruns.tf
@@ -1,9 +1,8 @@
 module "provisioning_server" {
   source          = "./modules/cloud-run"
-  name            = "sample-server"
+  name            = "sample-server"https://github.com/Yong-NNX/helloworld-infra/blob/main/cloudruns.tf
   service_version = var.service_version
   project_id      = var.service_project_id
   region          = var.region
-  invokers        = ["allUsers"]
 }
 
